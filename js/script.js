1909b19bd48954419a2bacb96a8fe3762c2203a6
Enzo <enzovizotto@byronsolutions.com>
2022-07-04 04:55:13 -0300

    slide

---
+++ js/script.js
@@ -0,0 +1,11 @@
+if(window.SimpleSlide) {
+    new SimpleSlide({
+        slide: "quote", // nome do atributo data-slide="principal"
+        time: 3000, // tempo de transição dos slides
+        nav: true
+    });
+}
+
+if(window.SimpleAnime) {
+    new SimpleAnime();
+}
