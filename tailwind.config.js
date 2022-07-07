39fae4c3fca88f6da668bdbd04869c51b5ec1ed8
Enzo <enzovizotto@byronsolutions.com>
2022-07-07 04:28:50 -0300

    iniciando responsivo

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,13 @@
 module.exports = {
   content: ["./src/**/*.{html, js}"],
   theme: {
+    screens: {
+      'mobile': {'max': '767px'},
+
+      'tablet': {'min': '768px', 'max': '979px'},
+
+      'note': {'min': '980px'},
+    },
     extend: {},
   },
   plugins: [],
